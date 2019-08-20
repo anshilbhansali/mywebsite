404e72e796a86c8935e6b3c2e0e610a00639eb64
anshil bhansali <anshilbhansali@anshils-MBP.fios-router.home>
2019-08-19 22:56:05 -0400

    front end makeover

--- app.py
+++ app.py
@@ -7,16 +7,43 @@ articles = {
 		1: "How I saved $100k at the age of 24",
 		2: "Why I am bullish about Slack",
 		3: "My biggest bet - Elasticsearch",
-		4: "Evolution of Operating Systems",
-		5: "Uber IPO",
-		6: "Analyzing a SAAS company",
-		7: "My take on personal finance",
-		8: "What the hell is the yield curve?"
+		4: "How I made a 60% return in 2 years",
+		5: "Evolution of Operating Systems",
+		6: "Uber IPO",
+		7: "Analyzing a SAAS company",
+		8: "Working in Chicago vs New York city",
+		9: "My take on personal finance",
+		10: "What the hell is the yield curve?"
 	}
 
+# PAGES 
 @app.route('/')
+def home():
+	return render_template('index.html', articles=articles)
+
+@app.route('/index')
 def index():
 	return render_template('index.html', articles=articles)
+
+@app.route('/technology')
+def technology():
+	return render_template('technology.html', articles=articles)
+
+@app.route('/current_markets')
+def current_markets():
+	return render_template('current_markets.html', articles=articles)
+
+@app.route('/personal_finance')
+def personal_finance():
+	return render_template('personal_finance.html', articles=articles)
+
+@app.route('/about')
+def about():
+	return render_template('about.html', articles=articles)
+
+@app.route('/contact')
+def contact():
+	return render_template('contact.html', articles=articles)
 
 @app.route('/article/<id>')
 def article(id):
@@ -36,4 +63,4 @@ def article(id):
 	return render_template('article.html', heading=articles[id], content=content, id=id)
 
 if __name__ == '__main__':
-	app.run()
+	app.run(debug=True)
